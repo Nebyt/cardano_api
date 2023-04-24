a78ed2b2470d608db32817fbacabf434b412aab1
Denis <nebytov.d@gmail.com>
2023-04-24 15:41:01 +0300

    return address

--- constants.js
+++ constants.js
@@ -1,2 +1,4 @@
 export const PREPROD_URL = 'https://preprod-backend.yoroiwallet.com/api'
 export const PREVIEW_URL = 'https://preview-backend.emurgornd.com/api'
+export const RETURN_ADDRESS =
+  'addr_test1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknswgndm3'
