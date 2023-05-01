d023861ecabd3eee4e48ecb3e1cb2e9948e2b7fb
Denis <nebytov.d@gmail.com>
2023-05-01 21:03:48 +0300

    added test for POST "/pool/info"

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 import supertest from 'supertest'
 import {assert} from 'chai'
+import Joi from 'joi'
 import {getTestnetURL} from './tools/helpers.js'
 import {fundInfoSchema} from './schemas/fundInfo-joi.js'
 import {pricesSchema} from './schemas/prices-joi.js'
-import Joi from 'joi'
 import {bestBlockSchema} from './schemas/bestblock-joi.js'
+import {poolInfoSchema} from './schemas/poolInfo-joi.js'
+import {POOL_APEX_CARDANO_HASH} from './constants.js'
 
 const request = supertest(getTestnetURL())
 
@@ -13,6 +15,7 @@ describe('Common API', () => {
     // Make a GET request to the bestblock route
     return request
       .get('/v2/bestblock')
+      .expect('Content-Type', /json/)
       .expect(200)
       .then((res) => {
         // assert data being return to not be empty
@@ -23,6 +26,7 @@ describe('Common API', () => {
   it('GET /v0/catalyst/fundInfo', () => {
     return request
       .get('/v0/catalyst/fundInfo')
+      .expect('Content-Type', /json/)
       .expect(200)
       .then((res) => {
         assert.isNotEmpty(res.body)
@@ -32,15 +36,27 @@ describe('Common API', () => {
   it('GET /price/ADA/current', () => {
     return request
       .get('/price/ADA/current')
+      .expect('Content-Type', /json/)
       .expect(200)
       .then((res) => {
         assert.isNotEmpty(res.body)
         Joi.assert(res.body, pricesSchema)
       })
   })
+  it('POST /pool/info', () => {
+    const payload = {poolIds: [POOL_APEX_CARDANO_HASH]}
+    return request
+      .post('/pool/info')
+      .send(payload)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        assert.isNotEmpty(res.body)
+        Joi.assert(res.body[POOL_APEX_CARDANO_HASH], poolInfoSchema)
+      })
+  })
   // POST /account/state {addresses:[...]}
   // POST /account/rewardHistory {addresses:[...]}
-  // POST /pool/info {poolIDs: ["8a77ce4ffc0c690419675aa5396df9a38c9cd20e36483d2d2465ce86"]}
   // POST /v2/addresses/filterUsed {addresses:[...]}
   // POST /v2/tipStatus {reference: {bestBlocks: [...]}}
   // GET /v2/tipStatus
