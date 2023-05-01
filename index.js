3841d68a92bc465934c0660bd1d7cfe8d21ff968
Denis <nebytov.d@gmail.com>
2023-05-01 21:02:24 +0300

    updated dummy index file

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import {
   getExternalAddrFromPrivateKey,
   getInternalAddrFromPrivateKey,
 } from './tools/wallet-tools.js'
-import {getBestBlock, getUtxos} from './api-requests.js'
+import {getBestBlock, getUtxos, getPoolInfo} from './api-requests.js'
+import {POOL_APEX_CARDANO_HASH} from './constants.js'
 
 // Getting PK and the base address
 const privateKey = getRootKeyFromMnemonic(
@@ -14,15 +15,27 @@ const privateKey = getRootKeyFromMnemonic(
 )
 const accountKey = getAccountKey(privateKey)
 const stakeKey = getStakeKey(accountKey)
-// const externalAddress = getExternalAddrFromPrivateKey(privateKey, stakeKey)
-const internalAddress = getInternalAddrFromPrivateKey(privateKey, stakeKey)
 
 // Requesting info
 const bestblock = await getBestBlock()
 console.log(`Best block response: ${JSON.stringify(bestblock, getCircularReplacer(), 2)}`)
-// const utxosForAddress = await getUtxos(externalAddress, bestblock.hash)
-const utxosForAddress = await getUtxos(internalAddress, bestblock.hash)
-console.log(`Utxos for address: ${JSON.stringify(utxosForAddress, getCircularReplacer(), 2)}`)
-const total = utxosForAddress.reduce((accumulator, utxo) => accumulator + parseInt(utxo.amount, 10), 0)
+
+// Requesting UTXOs for external address
+const externalAddress = getExternalAddrFromPrivateKey(privateKey, stakeKey)
+const utxosForExternalAddress = await getUtxos(externalAddress, bestblock.hash)
+console.log(`Utxos for external address: ${JSON.stringify(utxosForExternalAddress, getCircularReplacer(), 2)}`)
+
+// Requesting UTXOs for internal address
+const internalAddress = getInternalAddrFromPrivateKey(privateKey, stakeKey)
+const utxosForInternalAddress = await getUtxos(internalAddress, bestblock.hash)
+console.log(`Utxos for internal address: ${JSON.stringify(utxosForInternalAddress, getCircularReplacer(), 2)}`)
+
+// Calculating total amount in UTXOs on internal address
+const total = utxosForInternalAddress.reduce((accumulator, utxo) => accumulator + parseInt(utxo.amount, 10), 0)
 const parsedTotal = total / 1000000
 console.log(`Total: ${parsedTotal}`)
+
+// Requesting a pool info
+const poolHashes = [POOL_APEX_CARDANO_HASH]
+const poolInfo = await getPoolInfo(poolHashes)
+console.log(`Pools info: ${JSON.stringify(poolInfo, getCircularReplacer(), 2)}`)
