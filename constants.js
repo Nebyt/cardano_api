f1558c538eacec64a99505be5a4475c562b6402c
Denis <nebytov.d@gmail.com>
2023-05-01 21:01:37 +0300

    added pools constants

--- constants.js
+++ constants.js
@@ -4,6 +4,9 @@ export const PREVIEW_URL = 'https://preview-backend.emurgornd.com/api'
 export const RETURN_ADDRESS =
   'addr_test1qqr585tvlc7ylnqvz8pyqwauzrdu0mxag3m7q56grgmgu7sxu2hyfhlkwuxupa9d5085eunq2qywy7hvmvej456flknswgndm3'
 
+export const POOL_BAZAR_HASH = '8a77ce4ffc0c690419675aa5396df9a38c9cd20e36483d2d2465ce86'
+export const POOL_APEX_CARDANO_HASH = '7facad662e180ce45e5c504957cd1341940c72a708728f7ecfc6e349'
+
 export const BIP44_SCAN_SIZE = 20
 
 export const ChainDerivations = Object.freeze({
