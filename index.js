59d2e34f6e68a3a80f7d41fc02608cf8ac62eb14
Denis <nebytov.d@gmail.com>
2023-05-01 12:10:54 +0300

    updated index file

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import {getCircularReplacer} from './helpers.js'
-import {getRootKeyFromMnemonic, getAccountKey, getStakeKey, getAddrFromPrivateKey} from './wallet-tools.js'
+import {getCircularReplacer} from './tools/helpers.js'
+import {
+  getRootKeyFromMnemonic,
+  getAccountKey,
+  getStakeKey,
+  getExternalAddrFromPrivateKey,
+  getInternalAddrFromPrivateKey,
+} from './tools/wallet-tools.js'
 import {getBestBlock, getUtxos} from './api-requests.js'
 
 // Getting PK and the base address
@@ -8,13 +14,15 @@ const privateKey = getRootKeyFromMnemonic(
 )
 const accountKey = getAccountKey(privateKey)
 const stakeKey = getStakeKey(accountKey)
-const baseAddress = getAddrFromPrivateKey(privateKey, stakeKey)
+// const externalAddress = getExternalAddrFromPrivateKey(privateKey, stakeKey)
+const internalAddress = getInternalAddrFromPrivateKey(privateKey, stakeKey)
 
 // Requesting info
 const bestblock = await getBestBlock()
 console.log(`Best block response: ${JSON.stringify(bestblock, getCircularReplacer(), 2)}`)
-const utxosForAddress = await getUtxos(baseAddress, bestblock.hash)
+// const utxosForAddress = await getUtxos(externalAddress, bestblock.hash)
+const utxosForAddress = await getUtxos(internalAddress, bestblock.hash)
 console.log(`Utxos for address: ${JSON.stringify(utxosForAddress, getCircularReplacer(), 2)}`)
-const total = utxosForAddress.reduce((accumulator, utxo) => accumulator + parseInt(utxo.amount, 10), 0);
+const total = utxosForAddress.reduce((accumulator, utxo) => accumulator + parseInt(utxo.amount, 10), 0)
 const parsedTotal = total / 1000000
 console.log(`Total: ${parsedTotal}`)
