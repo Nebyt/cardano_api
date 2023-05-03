a89d48b2625e329256218451b637b4c894a24913
Denis <nebytov.d@gmail.com>
2023-05-03 23:21:08 +0300

    improved tipStatus schema

--- schemas/tipStatus-joi.js
+++ schemas/tipStatus-joi.js
@@ -2,6 +2,15 @@ import Joi from 'joi'
 import {blockSchema} from './block-joi.js'
 
 export const tipStatusSchema = Joi.object({
-  bestBlock: blockSchema,
-  safeBlock: blockSchema,
+  bestBlock: blockSchema.required(),
+  safeBlock: blockSchema.required(),
 })
+
+const referenceSchema = Joi.object({
+  reference: Joi.object({
+    lastFoundSafeBlock: Joi.string().hex().required(),
+    lastFoundBestBlock: Joi.string().hex().required(),
+  }),
+})
+
+export const tipStatusPostSchema = tipStatusSchema.concat(referenceSchema)
