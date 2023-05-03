f79f676fb5c6c14eebef1937df2e6b3e0f171404
Denis <nebytov.d@gmail.com>
2023-05-03 14:18:13 +0300

    added POST /account/rewardHistory tests

--- test/tx-releated-endpoints.js
+++ test/tx-releated-endpoints.js
@@ -14,6 +14,7 @@ import {accountStateSchema} from '../schemas/accountState-joi.js'
 import {blockSchema} from '../schemas/block-joi.js'
 import {tipStatusSchema} from '../schemas/tipStatus-joi.js'
 import {invalidHexadecimalDigitSchema, noAddressesSchema} from '../schemas/errors-joi.js'
+import {rewardHistorySchema} from '../schemas/rewardHistory-joi.js'
 
 const request = supertest(getTestnetURL())
 
@@ -125,6 +126,94 @@ describe('Tx related API', function () {
       .then((res) => {
         assert.isNotEmpty(res.body)
         Joi.assert(res.body, tipStatusSchema)
+      })
+  })
+
+  it('POST /account/rewardHistory, new empty wallet', async function () {
+    const newRecoveryPhrase = await generateWalletRecoveryPhrase()
+    const privateKey = getRootKeyFromMnemonic(newRecoveryPhrase)
+    const accountKey = getAccountKey(privateKey)
+    const stakeKey = getStakeKey(accountKey)
+    const rewardAddress = getRewardAddr(stakeKey)
+
+    // In case the payload is incorrect, e.g. the addresses are not in hex or it is not array the error 500 is returned
+    const rewardAddrHex = rewardAddress.to_hex()
+    const payload = {addresses: [rewardAddrHex]}
+
+    return request
+      .post('/account/rewardHistory')
+      .send(payload)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        assert.isNotEmpty(res.body)
+        Joi.assert(res.body[rewardAddrHex], rewardHistorySchema)
+      })
+  })
+
+  it('POST /account/rewardHistory, delegated wallet', async function () {
+    const privateKey = getRootKeyFromMnemonic(WALLET_DELEGATED)
+    const accountKey = getAccountKey(privateKey)
+    const stakeKey = getStakeKey(accountKey)
+    const rewardAddress = getRewardAddr(stakeKey)
+
+    // In case the payload is incorrect, e.g. the addresses are not in hex or it is not array the error 500 is returned
+    const rewardAddrHex = rewardAddress.to_hex()
+    const payload = {addresses: [rewardAddrHex]}
+
+    return request
+      .post('/account/rewardHistory')
+      .send(payload)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        assert.isNotEmpty(res.body)
+        Joi.assert(res.body[rewardAddrHex], rewardHistorySchema)
+      })
+  })
+
+  it('POST /account/rewardHistory, delegated wallet, bech32', async function () {
+    const privateKey = getRootKeyFromMnemonic(WALLET_DELEGATED)
+    const accountKey = getAccountKey(privateKey)
+    const stakeKey = getStakeKey(accountKey)
+    const rewardAddress = getRewardAddr(stakeKey)
+
+    // In case the payload is incorrect, e.g. the addresses are not in hex or it is not array the error 500 is returned
+    const rewardAddrHex = rewardAddress.to_bech32()
+    const payload = {addresses: [rewardAddrHex]}
+
+    return request
+      .post('/account/rewardHistory')
+      .send(payload)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((res) => {
+        assert.isNotEmpty(res.body)
+        Joi.assert(res.body[rewardAddrHex], rewardHistorySchema)
+      })
+  })
+
+  it('POST /account/rewardHistory, wrong payload, no payload', function () {
+    return request
+      .post('/account/rewardHistory')
+      .expect('Content-Type', /json/)
+      .expect(500)
+      .then((res) => {
+        assert.isNotEmpty(res.body)
+        Joi.assert(res.body, noAddressesSchema)
+      })
+  })
+
+  it('POST /account/rewardHistory, wrong payload, no addresses', function () {
+    const payload = {}
+    return request
+      .post('/account/rewardHistory')
+      .send(payload)
+      .expect('Content-Type', /json/)
+      .expect(500)
+      .then((res) => {
+        assert.isNotEmpty(res.body)
+        Joi.assert(res.body, noAddressesSchema)
       })
   })
 })
